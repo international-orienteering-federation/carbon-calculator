cbc83b60c05c2664a670e9e1b2e3067f02cb0591
Edoardo Tona <tona@edoar.do>
2023-01-17 18:41:43 +0100

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { StepperSelectionEvent, STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatAccordion } from '@angular/material/expansion';
+import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    }],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'carbon-calculator';
+  factors: string = "";
+
+
+  @ViewChild(MatAccordion) accordion: MatAccordion | undefined;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private httpclient: HttpClient) { }
+  ngOnInit(): void {
+
+    this.httpclient.get('assets/factors.csv', { responseType: 'text' })
+      .subscribe(
+        data => {
+          this.factors = data;
+        }
+      );
+
+  }
+  kmFormGroup: FormGroup = this._formBuilder.group({
+    km: [undefined, [
+      Validators.required,
+      Validators.pattern("^[0-9]*$"),
+      Validators.min(0),
+    ]]
+  });
+  engineFormGroup: FormGroup = this._formBuilder.group({ engine: ['', Validators.required] });
+  carFormGroup: FormGroup = this._formBuilder.group({ car: ['', Validators.required] });
+  peopleFormGroup: FormGroup = this._formBuilder.group({
+    people: [undefined, [
+      Validators.required,
+      Validators.pattern("^[0-9]*$"),
+      Validators.min(0),
+    ]]
+  });
+
+
+  tripFootprint: number = 0;
+  perCapitaFootprint: number = 0;
+
+  onStepChange(e: StepperSelectionEvent) {
+    if (e.selectedIndex == 5)
+      this.calculateFootprint();
+  }
+
+  onCarSelected(e: MatRadioChange) {
+    this.accordion?.closeAll();
+  }
+
+  calculateFootprint() {
+    let car = this.carFormGroup.controls['car'].value;
+    let engine = this.engineFormGroup.controls['engine'].value;
+
+
+    let km = this.kmFormGroup.controls['km'].value;
+    let people = this.peopleFormGroup.controls['people'].value;
+
+    let factor = 0;
+
+    for (let line of this.factors.split("\n")) {
+      if (!line.startsWith(`${car},${engine},`))
+        continue;
+
+      factor = parseFloat(line.split(',')[2]);
+    }
+
+    this.tripFootprint = km * factor;
+    this.perCapitaFootprint = this.tripFootprint / people;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
